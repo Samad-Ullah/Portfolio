6dcd72590b506cfe8aedeb2731b5352bce923b8f
samad Ullah <usamad152@gmail.com>
2021-06-29 12:31:09 +0500

    cards update

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './portfolio.scss';
 import Halaat from "../../Assets/Halaat.jpeg"
 import Amazon from "../../Assets/amazon.png"
-import Quiz from "../../Assets/quiz.jpg"
+import Quiz from "../../Assets/Quiz.png"
 import Flight from "../../Assets/flight.png"
-import Expense from "../../Assets/exp.jpg"
+import Expense from "../../Assets/pet1.jpg"
 
 function Portfolio() {
   return (
@@ -21,7 +21,7 @@ function Portfolio() {
           <h3>Halaat o Waqiat</h3>
         </div>
         </a>
-        
+        &nbsp;
       
         <a href="https://github.com/Samad-Ullah/Amazon-Clone">
         <div className="item">
@@ -29,25 +29,28 @@ function Portfolio() {
           <h3>Amazon Clone</h3>
         </div>
         </a>
-
+&nbsp;
         <a href="https://github.com/Samad-Ullah/Online_Quiz_Application">
         <div className="item">
           <img src={Quiz} alt="halat" ></img>
           <h3>Quiz Application</h3>
         </div>
         </a>
-
+&nbsp;
 
 
         <a href="https://github.com/Samad-Ullah/AirLine-Ticketing-UI">
+          
         <div className="item">
+        &nbsp;&nbsp;&nbsp;&nbsp;
           <img src={Flight} alt="halat" ></img>
           <h3>Airline Web Portal</h3>
         </div>
         </a>
-
+&nbsp;
         <a href="https://github.com/Samad-Ullah/Expense_Tracker_App">
         <div className="item">
+          &nbsp;&nbsp;&nbsp;&nbsp;
           <img src={Expense} alt="halat" height="150"></img>
           <h3>Expense Tracker</h3>
         </div>
