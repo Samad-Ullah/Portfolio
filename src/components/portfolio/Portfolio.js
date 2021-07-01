6360d9458ae05435f894e5706ad3fc216e987437
samad Ullah <usamad152@gmail.com>
2021-07-01 13:13:43 +0500

    cards edit

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -10,50 +10,50 @@ function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
       <h2>Web Projects</h2>
-     
+
       <div className="container">
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="https://halaatowaqiat.netlify.app/">
-        <div className="item">
-          <img src={Halaat} alt="halat"></img>
-          <h3>Halaat o Waqiat</h3>
-        </div>
+          <div className="item">
+            <img src={Halaat} alt="halat"></img>
+            <h3>Halaat o Waqiat</h3>
+          </div>
         </a>
         &nbsp;
-      
+
         <a href="https://github.com/Samad-Ullah/Amazon-Clone">
-        <div className="item">
-          <img src={Amazon} alt="halat" ></img>
-          <h3>Amazon Clone</h3>
-        </div>
+          <div className="item">
+            <img src={Amazon} alt="halat" ></img>
+            <h3>Amazon Clone</h3>
+          </div>
         </a>
-&nbsp;
+        &nbsp;
         <a href="https://github.com/Samad-Ullah/Online_Quiz_Application">
-        <div className="item">
-          <img src={Quiz} alt="halat" ></img>
-          <h3>Quiz Application</h3>
-        </div>
+          <div className="item">
+            <img src={Quiz} alt="halat" ></img>
+            <h3>Quiz Application</h3>
+          </div>
         </a>
-&nbsp;
+        &nbsp;
 
 
         <a href="https://github.com/Samad-Ullah/AirLine-Ticketing-UI">
-          
-        <div className="item">
-        &nbsp;&nbsp;&nbsp;&nbsp;
-          <img src={Flight} alt="halat" ></img>
-          <h3>Airline Web Portal</h3>
-        </div>
+
+          <div className="item">
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <img src={Flight} alt="halat" ></img>
+            <h3>Airline Web Portal</h3>
+          </div>
         </a>
-&nbsp;
+        &nbsp;
         <a href="https://github.com/Samad-Ullah/Expense_Tracker_App">
-        <div className="item">
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <img src={Expense} alt="halat" height="150"></img>
-          <h3>Expense Tracker</h3>
-        </div>
+          <div className="item">
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <img src={Expense} alt="halat" height="150"></img>
+            <h3>Expense Tracker</h3>
+          </div>
         </a>
-       
+
       </div>
     </div>
   )
